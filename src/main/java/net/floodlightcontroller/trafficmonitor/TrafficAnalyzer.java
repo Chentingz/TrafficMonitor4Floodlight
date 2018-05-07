321b19bc154f5c473a0ea43b8dddf647696cb362
Chentingz <ct2ing@gmail.com>
2018-05-08 01:23:10 +0800

    修改了一些参数名称，并添加了Policy参数，真正实现从从前端获取阈值来判断异常流量

--- src/main/java/net/floodlightcontroller/trafficmonitor/TrafficAnalyzer.java
+++ src/main/java/net/floodlightcontroller/trafficmonitor/TrafficAnalyzer.java
@@ -13,20 +13,22 @@ import org.slf4j.LoggerFactory;
 
 public class TrafficAnalyzer {
 	private static final Logger logger = LoggerFactory.getLogger(TrafficAnalyzer.class);
-	private static U64 threashold = U64.of(100);		// 阈值，单位Bps
-	
-	public static void Analysis(HashMap<NodePortTuple, SwitchPortStatistics> portStatsMap, HashSet<NodePortTuple> abnormalTrafficSet){
+
+	public static void Analysis(HashMap<NodePortTuple, SwitchPortStatistics> portStats, HashMap<NodePortTuple, SwitchPortStatistics> abnormalTraffic, Policy policy){
 		U64 portSpeed = U64.ZERO;
 		U64 rxSpeed = U64.ZERO;
 		U64 txSpeed = U64.ZERO;
-		for(Entry<NodePortTuple, SwitchPortStatistics> e : portStatsMap.entrySet()){
+		U64 portSpeedThreshold = policy.getPortSpeedThreshold();
+				
+		/* 根据端口流量与阈值比较来判断是否为异常流量 */
+		for(Entry<NodePortTuple, SwitchPortStatistics> e : portStats.entrySet()){
 			rxSpeed = e.getValue().getRxSpeed();
 			txSpeed = e.getValue().getTxSpeed();
 			
 			portSpeed = rxSpeed.add(txSpeed);
 			logger.info("" + e.getKey().getNodeId() +" / " + e.getKey().getPortId() + " portSpeed: " + portSpeed.getBigInteger().toString());
-			if(portSpeed.compareTo(threashold) > 0){
-				abnormalTrafficSet.add(e.getKey());
+			if(portSpeed.compareTo(portSpeedThreshold) > 0){
+				abnormalTraffic.put(e.getKey(), e.getValue());
 			}
 		}
 	}
