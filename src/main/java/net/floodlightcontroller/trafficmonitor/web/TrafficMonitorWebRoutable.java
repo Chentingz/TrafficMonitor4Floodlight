604537527b02607651e8688fec2fe38ec81c73b0
Chentingz <ct2ing@gmail.com>
2018-05-08 01:04:15 +0800

    添加查看事件的url路径，更新有关策略的url路径

--- src/main/java/net/floodlightcontroller/trafficmonitor/web/TrafficMonitorWebRoutable.java
+++ src/main/java/net/floodlightcontroller/trafficmonitor/web/TrafficMonitorWebRoutable.java
@@ -11,16 +11,15 @@ import net.floodlightcontroller.restserver.RestletRoutable;
 public class TrafficMonitorWebRoutable implements RestletRoutable {
 	protected static final String DPID_STR = "dpid";
 	protected static final String PORT_STR = "port";
-	protected static final String ACTION_DROP_STR = "drop";
-	protected static final String ACTION_RATE_LIMIT_STR = "rate-limit";
 		
 	@Override
 	public Restlet getRestlet(Context context) {
 		Router router = new Router(context);
 		/* 将url路径与处理方法绑定 */
 		router.attach("/portstats/{" + DPID_STR + "}/{" + PORT_STR + "}/json", PortStatsResource.class);
-		router.attach("/policyconf/json", PolicyConfigResource.class);
-		router.attach("/policyconf/list/json", ListPolicyConfigResource.class);
+		router.attach("/policy/conf/json", PolicyConfigResource.class);
+		router.attach("/policy/list/json", ListPolicyConfigResource.class);
+		router.attach("/events/json", ListEventsResource.class);
 		return router;
 	}
 
